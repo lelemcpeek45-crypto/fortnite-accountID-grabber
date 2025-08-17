771ec88cf1721508f9fddf56bc8a56eb76ae2e7f
lelemcpeek45-crypto <lelemcpeek45@gmail.com>
2025-08-16 23:10:21 -0500

    Update Grabber.py

--- Grabber.py
+++ Grabber.py
@@ -15,7 +15,7 @@ print("Coded by dev github/dev6920")
 
 while True:
   
-  username = input("Enter username (type 'exit' to quit): ")
+  username = input("Enter username (type 'exit' to quit): ") Sdot2comp
 
   if username == "exit":
     break
@@ -45,3 +45,4 @@ while True:
     print(f"Username: {player_name}")
   else:
     print("Cant find Account ID ")
+
